a88f5689eb5680b293e3d377ff183c9179383e70
Sanjarbek240 <ksanjarbek240@gmail.com>
2025-05-12 04:44:19 -0700

    .

---
+++ 25/3topshiriq/topshiriq.py
@@ -0,0 +1,22 @@
+import pickle
+
+with open("students.txt", "r") as file:
+    lines = file.readlines()
+students = {}
+for line in lines:
+    name, scores = line.strip().split(":")
+    score_list = list(map(int, scores.strip().split(",")))
+    students[name] = score_list
+
+with open("students.pkl", "wb") as pkl_file:
+    pickle.dump(students, pkl_file)
+with open("students.pkl", "rb") as pkl_file:
+    loaded_students = pickle.load(pkl_file)
+for name, scores in loaded_students.items():
+    avg = sum(scores) / len(scores)
+    print(f"{name}: ortacha bahosi = {avg:.2f}")
+ 
+
+# with open("results.txt", "w", encoding="utf-8") as file:
+#     for line in results:
+#         file.write(line + "\n")
